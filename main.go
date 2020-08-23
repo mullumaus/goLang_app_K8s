790d5819f2af811502767b501ef546427aa6a087
guolihui <lihuiguo@gmail.com>
2020-08-23 19:52:43 +1000

    add comments

--- main.go
+++ main.go
@@ -6,21 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//the value is assigned by ldflags in Makefile
 var (
 	version    string
 	lastcommit string
 )
 
+//create '/version' endpoint content
 func versionEndpoint(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"Version": version, "lastcommitsha": lastcommit, "description": "Simple Golang Application"})
 }
 
+//set up engine server
 func setupEngine() *gin.Engine {
 	r := gin.Default()
 	r.GET("/version", versionEndpoint)
 	return r
 }
 
+//main function
 func main() {
 	setupEngine().Run(":3000")
 }
